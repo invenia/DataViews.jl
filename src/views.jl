2656f67735a281504f10fdcf0acac85561c1be9e
Rory Finnegan <rory.finnegan@invenia.ca>
2015-12-02 15:06:25 -0500

    Initial attempt at getting index partitions working.
    Decided to replace the internal representation of the indices from using
    tuples of labels and expected values to just using an OrderedDict.
    
    Refactored some of the duplicate code in getindex and setindex! to minimize
    the work needed for this feature.
    
    Needs a lot more tests for each condition while still support both Dict and Tuple
    queries.

--- src/views.jl
+++ src/views.jl
@@ -1,6 +1,9 @@
-abstract AbstractDataView
+using DataStructures
 
 
+abstract AbstractDataView{I, T}
+
+typealias IndexValue Union{AbstractArray, }
 """
 `DataView{T<:AbstractDataCache}` provides a mechanism for arbitrary indexing strategies
 into an n-dimensional Array beyond just integer indexing. Similarly, these indices can be
@@ -13,16 +16,23 @@ Whenever possible DataViews will attempt to maintain the use of Ranges during su
 NOTE: The actual storage of the data is handled by the cache choice, which should minimize
 the likelihood of needing to create a custom view type.
 """
-immutable DataView{T<:AbstractDataCache} <: AbstractDataView
-    labels::Tuple{Vararg{Symbol}}
-    expected::Tuple
+immutable DataView{I<:Any, T<:AbstractDataCache} <: AbstractDataView{I,T}
+    index::OrderedDict{Symbol, I}
     cache::T
 end
 
 """
-`DataViews{T<:AbstractDataCache}(expected::Tuple, cache::T, label=())` is the default
-constructor that the other DataView constructors call which takes explicitly takes a
-DataCache.
+`DataViews{T<:AbstractDataCache}(index::OrderedDict{Symbol, Any}, cache::T` is the default
+constructor that the other DataView constructors call, which explicitly takes the index
+dict and DataCache.
+"""
+function DataView{I<:Any, T<:AbstractDataCache}(index::OrderedDict{Symbol, I}, cache::T)
+    DataView{I,T}(index, cache)
+end
+
+"""
+`DataViews{T<:AbstractDataCache}(expected::Tuple, cache::T; label=())` explicityly takes the
+cache, but allows optionally specifying the labels.
 """
 function DataView{T<:AbstractDataCache}(expected::Tuple, cache::T; labels=())
     if isempty(labels)
@@ -33,10 +43,30 @@ function DataView{T<:AbstractDataCache}(expected::Tuple, cache::T; labels=())
         error("You must provide labels for each key. labels=$labels; keys=$expected")
     end
 
-    DataView{T}(
-        labels,
-        expected,
+    @assert isa(labels, Tuple)
+    @assert isa(expected, Tuple)
+
+    DataView(
+        OrderedDict(zip(labels, expected)),
         cache
+    )
+end
+
+"""
+`DataView(index::Dict{Symbol, Any}; empty_value=0.0, mmapped=false)` is a
+helper constructor which will build a DefaultCache for you with the
+empty_value provided. If type of the empty_value`isbits` you can also tell
+it to build a memory mapped cache. Given that the default empty_value is 0.0
+this assumes that your cache type with only contain Floats.
+"""
+function DataView{I<:Any}(index::OrderedDict{Symbol, I}; empty_value=0.0, mmapped=false)
+    DataView(
+        index,
+        DataCache(
+            get_cache_dim(values(index))...;
+            empty_value=empty_value,
+            mmapped=mmapped
+        )
     )
 end
 
@@ -51,7 +81,7 @@ function DataView(expected::Tuple; labels=(), empty_value=0.0, mmapped=false)
     DataView(
         expected,
         DataCache(
-            map(i -> length(i), expected)...;
+            get_cache_dim(expected)...;
             empty_value=empty_value,
             mmapped=mmapped
         );
@@ -60,16 +90,90 @@ function DataView(expected::Tuple; labels=(), empty_value=0.0, mmapped=false)
 end
 
 """
+`DataView{T<:OnlineStat}(::Type{T}, index::Dict{Symbol, Any}; stats_dim=1)`
+builds a DataView with a `StatsCache` of type `T`.
+"""
+function DataView{T<:OnlineStat, I<:Any}(::Type{T}, index::OrderedDict{Symbol, I}; stats_dim=1, weighting=EqualWeighting())
+    cache = StatsCache(T, get_cache_dim(values(index))...; stats_dim=stats_dim, weighting=weighting)
+    DataView(
+        index,
+        cache
+    )
+end
+
+
+"""
 `DataView{T<:OnlineStat}(::Type{T}, expected::Tuple; labels=(), stats_dim=1)`
 builds a DataView with a `StatsCache` of type `T`.
 """
 function DataView{T<:OnlineStat}(::Type{T}, expected::Tuple; labels=(), stats_dim=1, weighting=EqualWeighting())
-    cache = StatsCache(T, map(i -> length(i), expected)...; stats_dim=stats_dim, weighting=weighting)
+    cache = StatsCache(T, get_cache_dim(expected)...; stats_dim=stats_dim, weighting=weighting)
     DataView(
         expected,
         cache;
         labels=labels
     )
+end
+
+# DOUBLE CHECK THIS! Was drunk when I wrote this
+# This should be recursive
+function indices(view::AbstractDataView, idx...)
+    index = Array{Base.ViewIndex, 1}(length(view.index))
+    fill!(index, :)
+
+    idx_vals = collect(values(view.index))
+
+    for i in eachindex(index)
+        tmp_exp = idx_vals[i]
+        tmp_idx = idx[i]
+
+        if !isa(tmp_idx, Colon)
+            if isa(tmp_idx, AbstractArray)
+                tmp_idx = findin(tmp_exp, tmp_idx)
+
+                if length(tmp_idx) == 0
+                    error("$(idx[i]) is not found in $(tmp_exp)")
+                end
+            elseif isa(tmp_idx, Tuple{Vararg{Symbol}}) || isa(tmp_idx, AbstractArray{Symbol})
+                if !isa(tmp_exp, OrderedDict)
+                    error("$tmp_idx only works for partition indices, but $(tmp_exp) is not a partitioned index")
+                end
+
+                tmp = Int[]
+                offset = 0
+                for k in keys(tmp_exp)
+                    if k in tmp_idx
+                        append!(tmp, offset + [1:length(tmp_exp[k])...])
+                    end
+                    offset += length(tmp_exp[k])
+                end
+                tmp_idx = tmp
+            elseif isa(tmp_idx, Associative)
+                if !isa(tmp_exp, OrderedDict{Symbol, Any})
+                    error("$(tmp_exp) is not a valid index argument")
+                end
+
+                tmp = Int[]
+                offset = 0
+                for k in keys(tmp_exp)
+                    if haskey(tmp_idx, k)
+                        append!(tmp, offset + [findin(tmp_exp[k], tmp_idx[k])...])
+                    end
+                    offset += length(tmp_exp[k])
+                end
+                tmp_idx = tmp
+            else
+                tmp_idx = findfirst(tmp_exp, tmp_idx)
+
+                if tmp_idx == 0
+                    error("$(idx[i]) not found in expected $tmp_exp")
+                end
+            end
+        end
+        index[i] = tmp_idx
+    end
+
+    return index
 end
 
 
@@ -80,16 +184,18 @@ by `keys(datum)` to select the cache index and the value to be
 inserted should be returned by `value(datum)`.
 """
 function Base.insert!(view::DataView, model::AbstractDatum)
-    index = Array{Any, 1}(length(view.labels))
+    index = Array{Any, 1}(length(view.index))
     fill!(index, -1)
 
+    idx_vals = collect(values(view.index))
+
     for i in eachindex(index)
-        idx = findfirst(view.expected[i], keys(model)[i])
+        idx = findfirst(idx_vals[i], keys(model)[i])
 
         if idx > 0
             index[i] = idx
         else
-            error("$(keys(model)[i]) is not in $(view.expected[i])")
+            error("$(keys(model)[i]) is not in $(idx_vals[i])")
         end
     end
 
@@ -103,27 +209,7 @@ using the expected keys to determine the determine the element(s) to
 set in the cache.
 """
 function Base.setindex!(view::DataView, x::Any, idx...)
-    index = Array{Any, 1}(length(view.labels))
-    fill!(index, :)
-
-    for i in eachindex(index)
-        tmp_exp = view.expected[i]
-        tmp_idx = idx[i]
-
-        if !isa(tmp_idx, Colon)
-            if isa(tmp_idx, Range) || !isa(tmp_idx, AbstractArray)
-                tmp_idx = findfirst(tmp_exp, tmp_idx)
-                if tmp_idx == 0
-                    error("$(idx[i]) not found in expected $tmp_exp")
-                end
-            else
-                tmp_idx = findin(tmp_exp, tmp_idx)
-            end
-        end
-
-        index[i] = tmp_idx
-    end
-
+    index = indices(view, idx...)
     view.cache[index...] = x
 end
 
@@ -135,23 +221,7 @@ from a sub cache rather than a copy of it. Also, if the subcache contains a
 single value then that value will be returned rather than a new DataView.
 """
 function Base.getindex(view::DataView, idx...)
-    index = Array{Base.ViewIndex, 1}(length(view.labels))
-    fill!(index, :)
-
-    for i in 1:length(idx)
-        tmp_exp = view.expected[i]
-        tmp_idx = idx[i]
-
-        if !isa(tmp_idx, Colon)
-            if isa(tmp_idx, Union{AbstractArray, Range})
-                tmp_idx = findin(tmp_exp, tmp_idx)
-            else
-                tmp_idx = findfirst(tmp_exp, tmp_idx)
-            end
-
-            index[i] = tmp_idx
-        end
-    end
+    index = indices(view, idx...)
 
     subcache = sub(view.cache, index...)
 
@@ -160,17 +230,31 @@ function Base.getindex(view::DataView, idx...)
     if length(subcache) == 1
         return subcache[1]
     else
-        new_exp = tuple(
-            map(
-                i -> view.expected[i][index[i]],
-                eachindex(index)
-            )...
-        )
+        idx_vals = collect(values(view.index))
+        new_exp = Any[]
+
+        for i in eachindex(index)
+            if isa(idx_vals[i], Associative)
+                sub_dict = OrderedDict()
+                if isa(idx[i], Tuple{Vararg{Symbol}}) || isa(idx[i], AbstractArray{Symbol})
+                    for key in idx[i]
+                        sub_dict[key] = idx_vals[i][key]
+                    end
+                elseif isa(idx[i], Associative)
+                    for key in keys(idx[i])
+                        sub_dic[key] = idx_vals[i][idx[i][key]]
+                    end
+                end
+                push!(new_exp, sub_dict)
+            else
+                push!(new_exp, idx_vals[i][index[i]])
+            end
+        end
 
         return DataView(
-            new_exp,
+            tuple(new_exp...),
             subcache;
-            labels=view.labels
+            labels=(keys(view.index)...)
         )
     end
 end
@@ -180,12 +264,12 @@ end
 `getindex(view::DataView, label::Symbol)` provides a mechanism for querying the
 `DataView` for its indices by label. NOTE: All labels are stored as Symbols.
 """
-Base.getindex(view::DataView, label::Symbol) = view.expected[findfirst(view.labels, label)]
+Base.getindex(view::DataView, label::Symbol) = view.index[label]
 
 """
 `data(view::DataView)` returns the labels, indices and cache as a tuple.
 """
-data(view::DataView) = view.labels, view.expected, view.cache
+data(view::DataView) = view.index, view.cache
 
 
 Base.insert!(view::AbstractDataView, x::Any) = error("Not Implemented")
@@ -193,3 +277,25 @@ Base.getindex(view::AbstractDataView, idx...) = error("Not Implemented")
 Base.getindex(view::AbstractDataView, label::Symbol) = error("Not Implemented")
 Base.setindex!(view::AbstractDataView, x::Any, idx...) = error("Not Implemented")
 data(view::AbstractDataView) = error("Not Implemeneted")
+
+
+function get_cache_dim(expected)
+    return map(expected) do d
+        l = 0
+
+        if isa(d, Associative)
+            for k in keys(d)
+                l += length(d[k])
+            end
+        else
+            for i in d
+                if isa(i, AbstractArray) || isa(i, Tuple)
+                    l += length(i)
+                else
+                    l += 1
+                end
+            end
+        end
+        return l
+    end
+end
--- test/views.jl
+++ test/views.jl
@@ -23,24 +23,38 @@ statsview = DataView(
 @test myview[:id] == expected_id
 
 myview[start, 1] = 5.0
-
 @test expected_time[1] == expected_time[1]
 
+# Test `insert!` with a datum
 datum = create_datum((expected_time[1], 2, 3.0))
-
 insert!(
     myview,
     datum
 )
 
-labels, expected, cache = data(myview)
-@test labels == (:time, :id)
-@test expected == (expected_time, expected_id)
+# Check index, cache tuple returned from `data`
+index, cache = data(myview)
+@test collect(keys(index)) == [:time, :id]
+@test collect(values(index)) == Array[expected_time, expected_id]
 @test cache[1,2] == 3.0
 
+# Check subselection
 subselect = myview[
     expected_time[2]:expected_time[4],
     2:5
 ]
 
-@test data(subselect)[3] == sub(data(myview)[3], 2:4, 2:5)
+@test data(subselect)[2] == sub(data(myview)[2], 2:4, 2:5)
+
+# Test partitioned DataView
+index_map = OrderedDict((
+    :time => expected_time,
+    :features => OrderedDict((
+        :gps => 1:5,
+        :direction => 1:8,
+        :weather => 1:10
+    ))
+))
+
+partitioned = DataView(index_map)
+@test data(partitioned[start, (:gps, :weather,)])[2] == zeros(15)'
